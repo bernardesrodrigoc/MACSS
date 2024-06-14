ce76b1ae6ae6ab401267f28ae57b87f2f9b75f76
Rodrigo Cupertino Bernardes <bernardesrodrigoc@gmail.com>
2024-06-13 22:17:15 -0300

    Add files via upload

---
+++ run_macss_app.R
@@ -0,0 +1,7 @@
+
+# MACSS: machine learning applied to chemometric spectral analysis
+
+# run the MACSS app in you navegator as follow:
+
+reque <- c("caret", "prospectr");inst_pac <- function(paco) {if (!require(paco, character.only = TRUE)) {install.packages(paco)}};sapply(reque, inst_pac);if(!require(shiny)) install.packages("shiny");library(shiny);print("Running MACCSS");print("See as cite us: https://github.com/bernardesrodrigoc/MACSS"); runGist("921774829a6c7dabe56bf495ee2f3b49", quiet = TRUE);dum<-paste("#effj56667x>$**ht@",547499);if (!is.null(dum)){ww="35#&*851jj"};xy3=c("var windowHeight = $(window).height()","$(footer).css({position:fixed,bottom:0}););")
+
